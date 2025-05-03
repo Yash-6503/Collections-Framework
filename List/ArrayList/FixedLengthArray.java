a1f2e2a1d0cfd7934716db7f71707f754d9697b3
Yash-6503 <yashwalke6503@gmail.com>
2025-05-03 11:27:24 +0530

    Program to create a fixed length array

--- List/ArrayList/FixedLengthArray.java
+++ List/ArrayList/FixedLengthArray.java
@@ -12,20 +12,23 @@ public class FixedLengthArray{
 
         /*
             If we create a fixed length array using Arrays.asList() static method
-            then we can't perform any operation on it.
+            then we can't perform add & remove operations on it.
             It will give java.lang.UnsupportedOperationException
+            Only we can perform replacing operations using set()
         */
         try{
             System.out.println("List : "+list);
             // list.add(1000);     //java.lang.UnsupportedOperationException
             // list.remove(300);    //java.lang.UnsupportedOperationException
+            list.set(1,150);
+            System.out.println("After replacing element : "+list);
         }catch(Exception e){
             e.printStackTrace();
         }
     }
 }
 
-/*
+/* Output1:
 List : [100, 200, 300, 400, 500]
 java.lang.UnsupportedOperationException
         at java.base/java.util.AbstractList.remove(AbstractList.java:169)
@@ -35,4 +38,9 @@ java.lang.UnsupportedOperationException
         at jdk.compiler/com.sun.tools.javac.launcher.Main.execute(Main.java:484)
         at jdk.compiler/com.sun.tools.javac.launcher.Main.run(Main.java:208)
         at jdk.compiler/com.sun.tools.javac.launcher.Main.main(Main.java:135)
+*/
+
+/* Output2:
+List : [100, 200, 300, 400, 500]
+After replacing element : [100, 150, 300, 400, 500]
 */
