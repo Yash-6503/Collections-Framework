6b9b69069804e6cf43dfd6e7f7feaa6abd7c10cb
Yash Walke <142409974+Yash-6503@users.noreply.github.com>
2025-12-12 19:49:57 +0530

    Program on PriorityQueue

--- Queue/PriorityQueue/PriorityQueueDemo.java
+++ Queue/PriorityQueue/PriorityQueueDemo.java
@@ -3,7 +3,7 @@
  * PriorityQueue stores elements in FIFO order.
  * It inserts element from last it is called as REAR.
  * It removes element from starting it is called as FRONT.
- * Elements in it are ordered according to their natural sorting order.
+ * Elements in it are ordered according to their natural order.
  * It does not permits/allows null elements.
  * Once one type element is inserted then another type elements are not allowed.
  * It stores the elements based on balanced binary heap tree.
@@ -12,7 +12,7 @@
 
 import java.util.*;
 
-public class InnerPriorityQueueDemo {
+public class PriorityQueueDemo {
     public static void main(String... args) {
         PriorityQueue<Object> pq = new PriorityQueue<>();
         pq.add("Orange");
