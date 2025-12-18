aa8419f0ff0ffcabb478546148a6727be8ec0a7c
Yash Walke <142409974+Yash-6503@users.noreply.github.com>
2025-12-19 00:08:28 +0530

    Find Two number sum

---
+++ Examples/TwoSum.java
@@ -0,0 +1,20 @@
+// Program to find sum of two numbers
+
+import java.util.Scanner;
+
+public class TwoSum {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        System.out.println("Enter first number : ");
+        int num1 = sc.nextInt();
+
+        System.out.println("Enter second number : ");
+        int num2 = sc.nextInt();
+
+        //first way to add 
+        System.out.println("Sum of " + num1 + " + " + num2 + " is : " + (num1 + num2));
+        
+        
+    }
+}
